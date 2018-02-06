2d67d5d998acc19485a54ccbd65250a51213dc5f
Louise1987 <31926492+Louise1987@users.noreply.github.com>
2018-02-06 16:01:19 +0100

    isset

--- header.php
+++ header.php
@@ -1,10 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Anton|Indie+Flower" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    
+    <title><?php 
+    
+    echo  SITE_NAME;
+    
+    if (isset($pageTitel)){
+        echo " - ".$pageTitle;
+    }
+    
+        
+    ?></title>
 
     <?php
 
--- index.php
+++ index.php
@@ -1,26 +1,15 @@
 
 <!DOCTYPE html>
 <?php
-include "data.php";
-?>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://fonts.googleapis.com/css?family=Anton|Indie+Flower" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <title><?php 
-    
-    echo  SITE_NAME;
-    
-    if (isset($pageTitel)){
-        echo " - ".$pageTitle;
-    }
-    
-        include "./header.php";
-    ?></title>
 
+define("SITE_NAME", "Mina filmer");
+
+include "header.php";
+?>
+
+
+<head>
+  
    
 
 
